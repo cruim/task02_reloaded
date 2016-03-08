094d6aabe87a82007b929d4c5bfde3cb4ae1b95a
Daniil Arashkevich <daniil.arashkevich@gmail.com>
2016-03-08 20:44:26 +0300

    init commit

---
+++ Task_02/Matrix/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину массива: ");
+            int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
+            Console.Write("Введите ширину массива: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = new int[n, m]; //Объявляем массив строк длиной n и шириной m (которую ввёл пользователь)
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                metka:
+                    try
+                    {
+                        Console.Write("Введите элемент №{0}{1}:\r\n    ", i, j);
+                        matrix[i, j] = Convert.ToInt32(Console.ReadLine()); //Заполняем его
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Вводите только целые числа!");
+                        goto metka;
+                    }
+                }
+            PrintArray(matrix);
+
+            int minI = 0, minJ = 0, maxI = 0, maxJ = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[minI, minJ] > matrix[i, j])
+                    {
+                        minI = i;
+                        minJ = j;
+                    }
+                    else if (matrix[maxI, maxJ] < matrix[i, j])
+                    {
+                        maxI = i;
+                        maxJ = j;
+                    }
+                }
+            }
+
+            Console.WriteLine("Минимальный элемент: {0}\tМаксимальный максимальный: {1}", matrix[minI, minJ], matrix[maxI, maxJ]);
+
+            var tmp = matrix[maxI, maxJ];
+            matrix[maxI, maxJ] = matrix[minI, minJ];
+            matrix[minI, minJ] = tmp;
+
+            PrintArray(matrix);
+            Console.ReadKey();
+        }
+
+        static void PrintArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+ * уточнить по использованию локальных переменных, размещенных в блоке try
+ * за переделами блока
+*/
---
+++ Task_02/Task_02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = new char[] { 'd', 'f', 'a', 'q', 'w', 'e', 'r', 't', 'y', ' ' };
+
+            string s = new string(arr);
+            StringBuilder sb = new StringBuilder(s);
+            Console.WriteLine(sb);
+
+            sb.Replace('d', 'm');
+            Console.WriteLine(sb);
+
+            sb.Remove(0, 3);
+            Console.WriteLine(sb);
+
+            sb.Insert(0, "csharp ");
+            Console.WriteLine(sb);
+
+            Console.ReadKey();
+        }
+    }
+}
